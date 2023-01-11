ad7db5aa0bd84dee127f0c37cee3308093c1c60f
BECREATOR/aki.zheng <ahsi.aki@gmail.com>
2023-01-11 17:11:25 +0800

    feat: realtime test and link backend api

--- src/components/HelloWorld.spec.js
+++ src/components/HelloWorld.spec.js
@@ -1,5 +1,5 @@
 // 測 Hellow World 有沒有包含 '現在開始' 的文字
-import { shollowMount, mount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import HelloWord from "@/components/HelloWorld.vue"
 import { describe, expect, it } from 'vitest'
 import vuetify from '@/plugins/vuetify'
@@ -12,9 +12,7 @@ describe("測試了 HelloWorld.vue", () => {
         plugins: [vuetify]
       }
     })
-
-    // console.log(wrapper.text()) // 印出元件內的所有文字
-    expect(wrapper.text()).toMatch("現在開始")
+    expect(wrapper.text()).toMatch(/現在開始/)
   })
 })
 
