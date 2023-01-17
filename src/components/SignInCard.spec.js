236a261262fe26576f992e8ae740bcfde48ac15a
BECREATOR/aki.zheng <ahsi.aki@gmail.com>
2023-01-17 09:08:10 +0800

    vitest setup

--- src/components/SignInCard.spec.js
+++ src/components/SignInCard.spec.js
@@ -1,18 +1,18 @@
 // 測 Hellow World 有沒有包含 '現在開始' 的文字
 import { shallowMount, mount } from '@vue/test-utils'
-import HelloWord from "@/components/HelloWorld.vue"
+import SignInCardVue from './SignInCard.vue'
 import { describe, expect, it } from 'vitest'
 import vuetify from '@/plugins/vuetify'
 
 
-describe("測試了 HelloWorld.vue", () => {
+describe("測試了 SignInCard.vue", () => {
   it('渲染屬性的訊息', () => {
-    const wrapper = mount(HelloWord, {
+    const wrapper = mount(SignInCardVue, {
       global: {
         plugins: [vuetify]
       }
     })
-    expect(wrapper.text()).toMatch(/現在開始/)
+    expect(wrapper.text()).toMatch(/Google/)
   })
 })
 
